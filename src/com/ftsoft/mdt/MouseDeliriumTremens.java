bc78092b112597535eb202d4b9f3926120b47f2c
tferrara3 <ftsoft81@gmail.com>
2023-09-11 15:18:26 +0200

    Update MouseDeliriumTremens.java

--- src/com/ftsoft/mdt/MouseDeliriumTremens.java
+++ src/com/ftsoft/mdt/MouseDeliriumTremens.java
@@ -1,5 +1,6 @@
 package com.ftsoft.mdt;
 
+import java.awt.AWTException;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
@@ -12,28 +13,35 @@ public class MouseDeliriumTremens {
 	// where it already is.
 	// From a user perspective the mouse doesn't move, but it does prevent the
 	// computer from going idle.
-	public static void main(String... args) throws Exception {
-		Robot robot = new Robot();
-		Point pointOld= null;
-		while (true) {
-			if (checkTimeInterval()) {				
-				Point point = MouseInfo.getPointerInfo().getLocation();
-				if (pointOld==null || !point.equals(pointOld)) {
-					robot.mouseMove(point.x, point.y);
-					pointOld= point;
-					System.out.println("Mouse Moved!!");
+	public static void main(String[] args) {
+		Robot robot;
+		try {
+			robot = new Robot();		
+			Point pointOld= null;
+			while (true) {
+				if (checkTimeInterval()) {				
+					Point point = MouseInfo.getPointerInfo().getLocation();
+					if (pointOld==null || !point.equals(pointOld)) {
+						robot.mouseMove(point.x, point.y+1);
+						pointOld= point;
+						//System.out.println("Mouse Moved!!");
+					}
+				} else {
+					//System.out.println("Not in time interval");
 				}
-			} else {
-				System.out.println("Not in time interval");
+				Thread.sleep(SLEEP_MILLIS);
 			}
-			Thread.sleep(SLEEP_MILLIS);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private static boolean checkTimeInterval() {
 		Calendar cal = Calendar.getInstance();
 		int hour = cal.get(Calendar.HOUR_OF_DAY);
-		int minutes = cal.get(Calendar.MINUTE);
+		//int minutes = cal.get(Calendar.MINUTE);
 		return hour >= 8 && hour <= 18;
 	}
 }
